a4a0b010e99be4ba845368eed4e18933d70e7ccb
ParkHyeonSeong <yolo.xsha@gmail.com>
2018-07-11 11:47:03 +0900

    2018.07.11. New Solved!

--- acmicpc.net/15552.py
+++ acmicpc.net/15552.py
@@ -1,11 +1,9 @@
 import sys
-
 c = sys.stdin.readline().rstrip()
-t = []
-
 for i in range(0,int(c)):
-    a,b = int(sys.stdin.readline().rstrip().split(" "))
-    t.append(a+b)
+    a,b = sys.stdin.readline().rstrip().split(" ")
+    sys.stdout.write(str(int(a)+int(b))+"\n")
 
-for i in range(0,len(t)):
-    print(t[i])
+# 뻘짓하다가 메모리초과만 10번가까이 냈다
+# 이유는 입력을 받고 저장하지않고 바로 더한 값을 출력하면 빠르게 진행된다
+# 문제를 다른 방식으로 접근하는 눈을 가져야겠다.
