446bf9575fec37ebea18466d471b75299a15fd38
ParkHyeonSeong <yolo.xsha@gmail.com>
2018-07-15 21:37:19 +0900

    2018.07.15. New Solved!

--- acmicpc.net/1463.py
+++ acmicpc.net/1463.py
@@ -1,16 +1,13 @@
 n = int(input())
-_min = []
-
-for i in range(n+1):
-    _min.append(0)
-_min[1] = 0 # 조건 명시
-_min[2] = 1
-_min[3] = 1
+_min = [n]
+_min.append(0)  # 조건 명시
+_min.append(1)  # 조건 명시
+_min.append(1)  # 조건 명시
 
 temp = 0    # 비교를 위한 임시 저장
 
 for i in range(4,n+1):
-    _min[i] = _min[i-1] + 1 # 최대횟수 설정
+    _min.append(_min[i-1] + 1) # 최대횟수 설정
 
     if i % 2 == 0:  # 나눠질때
         temp = _min[i // 2] + 1  # 자신이 나뉘어진 숫자의 최소횟수보다
