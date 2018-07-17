6b1897dfc54f353466c5e18dca034ecdd1b143ee
ParkHyeonSeong <yolo.xsha@gmail.com>
2018-07-17 23:45:58 +0900

    2018.07.17. 막대기(성공)

--- acmicpc.net/1094.py
+++ acmicpc.net/1094.py
@@ -1,20 +1,21 @@
-_in = int(input())  # 최종값을 입력 받음
+x = int(input())  # 최종값을 입력 받음
 
-have = [64] # 가지고 있는 막대를 리스트에 추가함
+have = [0,64] # 가지고 있는 막대를 리스트에 추가함(0은 논리적으로 쉬우라고..)
 total = 64  # 막대기 길이의 총합
-
 temp = 0    # 임시 저장
 count = 1   # 카운트
 
-while total >= _in:  # 막대의 합이 최종값보다 같거나 클때까지 반복
-    temp = have.pop()   # 마지막 값을 팝!
-    print(temp)
-    have.append(temp/2) # 막대기를 두동강 낸 후 하나를 저장하고
-    have.append(temp/2/2) # 절반 중 절반을 하나 버리고 하나를 저장
 
-    total = sum(have)   # 막대기를 모두 붙임
-    count += 1  # 카운트++
+while total > x:  # 막대의 합이 최종값보다 같거나 클때까지 반복
+    temp = have.pop()       # 마지막 값을 팝!
+    have.append(temp//2)    # 막대기를 절반하여 넣음
+    have.append(temp//2)    # 막대기를 절반하여 넣음 2
+    count += 1              # 결과적으로 막대기 수가 1개 늘음
 
-    print(total)
+    if sum(have) - have[count] >= x:    # 자른 것중 1개를 버리고 남은 막대의 길이가 x보다 크거나 같다면
+        del have[count] # 막대기하나를 버림
+        count -= 1
+
+    total = sum(have)       # 살아남은 막대기를 모두 붙임
 
 print(count)
